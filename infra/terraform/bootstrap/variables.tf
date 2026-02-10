c1d82ad8f84d1f521aca18d3c05268423df7b824
Travis S <mcderkaderka1@gmail.com>
2026-02-09 22:11:25 -0500

    feat(s3): add lifecycle policy for artifacts bucket (dev)

--- infra/terraform/bootstrap/variables.tf
+++ infra/terraform/bootstrap/variables.tf
@@ -15,13 +15,3 @@ variable "env" {
   description = "Environment name"
   default     = "dev"
 }
-
-variable "tf_state_bucket_name" {
-  type        = string
-  description = "Globally-unique S3 bucket name for Terraform remote state"
-}
-
-variable "tf_lock_table_name" {
-  type        = string
-  description = "DynamoDB table name for Terraform state locking"
-}
